5ada9b9bf2e9f58d9f0a9f1504a49d61bf19e9a2
MarshandaGra <marshandagracella.15@gmail.com>
2026-09-26 06:11:14 +0700

    fresh

--- resources/views/guru/materi/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('guru.layouts.app')
-@section('container')
-    <h1>Materi</h1>
-    <p>Selamat datang di halaman materi!</p>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('guru.dashboard');
-});
-
 
